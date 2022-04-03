968b09f80aa7071b0cd051e1d3fff65cebdc4258
Misha Chuzov <chuzovm@mail.ru>
2022-04-03 18:40:07 +0300

    add sign in and sign out

---
+++ login.js
@@ -0,0 +1,43 @@
+baseurl = 'http://130.162.173.167/api/'
+
+function login(){
+  var login = document.getElementById('login').value;
+  var passw = document.getElementById('passw').value;
+  var params = {}
+  params['username'] = login;
+  params['password'] = passw;
+  // axios.post( baseurl + 'auth/login', {params:params} )
+  // .then( r => console.log(r) );
+  console.log(baseurl + 'auth/login');
+  axios.post(baseurl + 'auth/login', params).then(function(r) {
+    console.log(r);
+    if( r.status == 200 ){
+      if( r['data'].status_code == 200 ){
+        localStorage.setItem( 'token', r['data']['data']['token'] );
+        localStorage.setItem( 'name', r['data']['data']['user']['username'] );
+
+        window.location.href = 'test.html';
+      }
+
+    }
+    //code here
+});
+}
+
+
+
+function logout(){
+
+  config = {}
+  console.log("logout");
+  axios.post(baseurl + 'auth/logout',{},{
+    headers: {
+        'Authorization':'Token ' + localStorage.getItem('token')
+    }
+  }).then(function(r){
+    localStorage.removeItem('token');
+    localStorage.removeItem('name');
+    window.location.href = 'test.html';
+  });
+
+}
---
+++ user.js
@@ -0,0 +1,32 @@
+baseurl = 'http://130.162.173.167/api/'
+
+function is_user(){
+
+  var token = localStorage.getItem('token');
+  var name = localStorage.getItem('name');
+  console.log( token );
+  if( token == null ){
+    var u = document.getElementById('user');
+    var login = document.createElement("a");
+    login.setAttribute('href','sign_in.html');
+    // console.log( u );
+    login.innerHTML = "Войти или Зарегестрироваться";
+    u.appendChild( login );
+  } else {
+    var u = document.getElementById('user');
+    var login = document.createElement("button");
+    // login.setAttribute('href','#');
+    login.setAttribute('onclick','logout()');
+    login.innerHTML = "Выйти";
+    u.appendChild( login );
+  }
+
+}
+
+
+
+
+
+window.onload = function() {
+  is_user();
+}
